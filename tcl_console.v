ffcedaa84d09c609fc92b3715ab91b6ce6b9e5fc
Snehita Patta <snehitapatta624@gmail.com>
2026-06-06 01:04:40 +0530

    tcl_console.v

---
+++ tcl_console.v
@@ -0,0 +1,75 @@
+t=0 | ps=000|ns=000|wr_en=0|data_in=00||tx_data_reg=00 | fifo_data_out=00|fifo_empty=1 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=120000 | ps=000|ns=000|wr_en=1|data_in=aa||tx_data_reg=00 | fifo_data_out=00|fifo_empty=1 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=125000 | ps=000|ns=001|wr_en=1|data_in=aa||tx_data_reg=00 | fifo_data_out=00|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=130000 | ps=000|ns=001|wr_en=1|data_in=bb||tx_data_reg=00 | fifo_data_out=00|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=135000 | ps=001|ns=010|wr_en=1|data_in=bb||tx_data_reg=00 | fifo_data_out=00|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=140000 | ps=001|ns=010|wr_en=1|data_in=cc||tx_data_reg=00 | fifo_data_out=00|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=145000 | ps=010|ns=011|wr_en=1|data_in=cc||tx_data_reg=00 | fifo_data_out=00|fifo_empty=0 | rd_en=1 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=150000 | ps=010|ns=011|wr_en=1|data_in=dd||tx_data_reg=00 | fifo_data_out=00|fifo_empty=0 | rd_en=1 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=155000 | ps=011|ns=100|wr_en=1|data_in=dd||tx_data_reg=00 | fifo_data_out=aa|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=160000 | ps=011|ns=100|wr_en=0|data_in=dd||tx_data_reg=00 | fifo_data_out=aa|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=165000 | ps=100|ns=101|wr_en=0|data_in=dd||tx_data_reg=aa | fifo_data_out=aa|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=175000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=aa | fifo_data_out=aa|fifo_empty=0 | rd_en=0 | tx_start=1 |tx_busy=0| tx_done=0| tx=1 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=185000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=aa | fifo_data_out=aa|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=0 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=265000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=aa | fifo_data_out=aa|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=1 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=305000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=aa | fifo_data_out=aa|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=0 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=345000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=aa | fifo_data_out=aa|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=1 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=385000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=aa | fifo_data_out=aa|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=0 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=425000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=aa | fifo_data_out=aa|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=1 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=465000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=aa | fifo_data_out=aa|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=0 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=505000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=aa | fifo_data_out=aa|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=1 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=545000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=aa | fifo_data_out=aa|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=0 |tx_count=  0|data_out=00|,rx_done=0|,parity_error=0|,framing_error=0
+t=585000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=aa | fifo_data_out=aa|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=1 |tx_count=  1|data_out=00|,rx_done=1|,parity_error=0|,framing_error=0
+t=615000 | ps=101|ns=000|wr_en=0|data_in=dd||tx_data_reg=aa | fifo_data_out=aa|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=1| tx=1 |tx_count=  1|data_out=00|,rx_done=1|,parity_error=0|,framing_error=0
+t=625000 | ps=000|ns=001|wr_en=0|data_in=dd||tx_data_reg=aa | fifo_data_out=aa|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  1|data_out=aa|,rx_done=0|,parity_error=0|,framing_error=0
+t=635000 | ps=001|ns=010|wr_en=0|data_in=dd||tx_data_reg=aa | fifo_data_out=aa|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  1|data_out=aa|,rx_done=0|,parity_error=0|,framing_error=0
+t=645000 | ps=010|ns=011|wr_en=0|data_in=dd||tx_data_reg=aa | fifo_data_out=aa|fifo_empty=0 | rd_en=1 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  1|data_out=aa|,rx_done=0|,parity_error=0|,framing_error=0
+t=655000 | ps=011|ns=100|wr_en=0|data_in=dd||tx_data_reg=aa | fifo_data_out=bb|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  1|data_out=aa|,rx_done=0|,parity_error=0|,framing_error=0
+t=665000 | ps=100|ns=101|wr_en=0|data_in=dd||tx_data_reg=bb | fifo_data_out=bb|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  1|data_out=aa|,rx_done=0|,parity_error=0|,framing_error=0
+t=675000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=bb | fifo_data_out=bb|fifo_empty=0 | rd_en=0 | tx_start=1 |tx_busy=0| tx_done=0| tx=1 |tx_count=  1|data_out=aa|,rx_done=0|,parity_error=0|,framing_error=0
+t=685000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=bb | fifo_data_out=bb|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=0 |tx_count=  1|data_out=aa|,rx_done=0|,parity_error=0|,framing_error=0
+t=705000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=bb | fifo_data_out=bb|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=1 |tx_count=  1|data_out=aa|,rx_done=0|,parity_error=0|,framing_error=0
+t=785000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=bb | fifo_data_out=bb|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=0 |tx_count=  1|data_out=aa|,rx_done=0|,parity_error=0|,framing_error=0
+t=825000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=bb | fifo_data_out=bb|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=1 |tx_count=  1|data_out=aa|,rx_done=0|,parity_error=0|,framing_error=0
+t=945000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=bb | fifo_data_out=bb|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=0 |tx_count=  1|data_out=aa|,rx_done=0|,parity_error=0|,framing_error=0
+t=985000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=bb | fifo_data_out=bb|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=1 |tx_count=  1|data_out=aa|,rx_done=0|,parity_error=0|,framing_error=0
+INFO: [USF-XSim-96] XSim completed. Design snapshot 'UART_FIFO_TX_TOP_tb_behav' loaded.
+INFO: [USF-XSim-97] XSim simulation ran for 1000ns
+launch_simulation: Time (s): cpu = 00:00:03 ; elapsed = 00:00:05 . Memory (MB): peak = 1717.672 ; gain = 4.137
+run all
+t=1025000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=bb | fifo_data_out=bb|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=0 |tx_count=  1|data_out=aa|,rx_done=0|,parity_error=0|,framing_error=0
+t=1065000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=bb | fifo_data_out=bb|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=1 |tx_count=  2|data_out=aa|,rx_done=1|,parity_error=0|,framing_error=0
+t=1095000 | ps=101|ns=000|wr_en=0|data_in=dd||tx_data_reg=bb | fifo_data_out=bb|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=1| tx=1 |tx_count=  2|data_out=aa|,rx_done=1|,parity_error=0|,framing_error=0
+t=1105000 | ps=000|ns=001|wr_en=0|data_in=dd||tx_data_reg=bb | fifo_data_out=bb|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  2|data_out=bb|,rx_done=0|,parity_error=0|,framing_error=0
+t=1115000 | ps=001|ns=010|wr_en=0|data_in=dd||tx_data_reg=bb | fifo_data_out=bb|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  2|data_out=bb|,rx_done=0|,parity_error=0|,framing_error=0
+t=1125000 | ps=010|ns=011|wr_en=0|data_in=dd||tx_data_reg=bb | fifo_data_out=bb|fifo_empty=0 | rd_en=1 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  2|data_out=bb|,rx_done=0|,parity_error=0|,framing_error=0
+t=1135000 | ps=011|ns=100|wr_en=0|data_in=dd||tx_data_reg=bb | fifo_data_out=cc|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  2|data_out=bb|,rx_done=0|,parity_error=0|,framing_error=0
+t=1145000 | ps=100|ns=101|wr_en=0|data_in=dd||tx_data_reg=cc | fifo_data_out=cc|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  2|data_out=bb|,rx_done=0|,parity_error=0|,framing_error=0
+t=1155000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=cc | fifo_data_out=cc|fifo_empty=0 | rd_en=0 | tx_start=1 |tx_busy=0| tx_done=0| tx=1 |tx_count=  2|data_out=bb|,rx_done=0|,parity_error=0|,framing_error=0
+t=1165000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=cc | fifo_data_out=cc|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=0 |tx_count=  2|data_out=bb|,rx_done=0|,parity_error=0|,framing_error=0
+t=1265000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=cc | fifo_data_out=cc|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=1 |tx_count=  2|data_out=bb|,rx_done=0|,parity_error=0|,framing_error=0
+t=1345000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=cc | fifo_data_out=cc|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=0 |tx_count=  2|data_out=bb|,rx_done=0|,parity_error=0|,framing_error=0
+t=1425000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=cc | fifo_data_out=cc|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=1 |tx_count=  2|data_out=bb|,rx_done=0|,parity_error=0|,framing_error=0
+t=1505000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=cc | fifo_data_out=cc|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=0 |tx_count=  2|data_out=bb|,rx_done=0|,parity_error=0|,framing_error=0
+t=1545000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=cc | fifo_data_out=cc|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=1 |tx_count=  3|data_out=bb|,rx_done=1|,parity_error=0|,framing_error=0
+t=1575000 | ps=101|ns=000|wr_en=0|data_in=dd||tx_data_reg=cc | fifo_data_out=cc|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=1| tx=1 |tx_count=  3|data_out=bb|,rx_done=1|,parity_error=0|,framing_error=0
+t=1585000 | ps=000|ns=001|wr_en=0|data_in=dd||tx_data_reg=cc | fifo_data_out=cc|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  3|data_out=cc|,rx_done=0|,parity_error=0|,framing_error=0
+t=1595000 | ps=001|ns=010|wr_en=0|data_in=dd||tx_data_reg=cc | fifo_data_out=cc|fifo_empty=0 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  3|data_out=cc|,rx_done=0|,parity_error=0|,framing_error=0
+t=1605000 | ps=010|ns=011|wr_en=0|data_in=dd||tx_data_reg=cc | fifo_data_out=cc|fifo_empty=0 | rd_en=1 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  3|data_out=cc|,rx_done=0|,parity_error=0|,framing_error=0
+t=1615000 | ps=011|ns=100|wr_en=0|data_in=dd||tx_data_reg=cc | fifo_data_out=dd|fifo_empty=1 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  3|data_out=cc|,rx_done=0|,parity_error=0|,framing_error=0
+t=1625000 | ps=100|ns=101|wr_en=0|data_in=dd||tx_data_reg=dd | fifo_data_out=dd|fifo_empty=1 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  3|data_out=cc|,rx_done=0|,parity_error=0|,framing_error=0
+t=1635000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=dd | fifo_data_out=dd|fifo_empty=1 | rd_en=0 | tx_start=1 |tx_busy=0| tx_done=0| tx=1 |tx_count=  3|data_out=cc|,rx_done=0|,parity_error=0|,framing_error=0
+t=1645000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=dd | fifo_data_out=dd|fifo_empty=1 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=0 |tx_count=  3|data_out=cc|,rx_done=0|,parity_error=0|,framing_error=0
+t=1665000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=dd | fifo_data_out=dd|fifo_empty=1 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=1 |tx_count=  3|data_out=cc|,rx_done=0|,parity_error=0|,framing_error=0
+t=1705000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=dd | fifo_data_out=dd|fifo_empty=1 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=0 |tx_count=  3|data_out=cc|,rx_done=0|,parity_error=0|,framing_error=0
+t=1745000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=dd | fifo_data_out=dd|fifo_empty=1 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=1 |tx_count=  3|data_out=cc|,rx_done=0|,parity_error=0|,framing_error=0
+t=1865000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=dd | fifo_data_out=dd|fifo_empty=1 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=0 |tx_count=  3|data_out=cc|,rx_done=0|,parity_error=0|,framing_error=0
+t=1905000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=dd | fifo_data_out=dd|fifo_empty=1 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=1 |tx_count=  3|data_out=cc|,rx_done=0|,parity_error=0|,framing_error=0
+t=1985000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=dd | fifo_data_out=dd|fifo_empty=1 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=0 |tx_count=  3|data_out=cc|,rx_done=0|,parity_error=0|,framing_error=0
+t=2025000 | ps=101|ns=101|wr_en=0|data_in=dd||tx_data_reg=dd | fifo_data_out=dd|fifo_empty=1 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=0| tx=1 |tx_count=  4|data_out=cc|,rx_done=1|,parity_error=0|,framing_error=0
+t=2055000 | ps=101|ns=000|wr_en=0|data_in=dd||tx_data_reg=dd | fifo_data_out=dd|fifo_empty=1 | rd_en=0 | tx_start=0 |tx_busy=1| tx_done=1| tx=1 |tx_count=  4|data_out=cc|,rx_done=1|,parity_error=0|,framing_error=0
+t=2065000 | ps=000|ns=000|wr_en=0|data_in=dd||tx_data_reg=dd | fifo_data_out=dd|fifo_empty=1 | rd_en=0 | tx_start=0 |tx_busy=0| tx_done=0| tx=1 |tx_count=  4|data_out=dd|,rx_done=0|,parity_error=0|,framing_error=0
+reached 2ms
+reached 4ms
+reached 6ms
+$finish called at time : 6000160 ns : File "C:/Users/snehi/OneDrive/Desktop/FSM_Based_UART_Communication_System_with_TX_RX/FSM_Based_UART_Communication_sSstem_with_TX_RX.srcs/sim_1/new/UART_FIFO_TX_TOP_tb.v" Line 61
